5a04609e219927cd810336906b3633829c558cf8
Joao Carlos <joaocsilva2@gmail.com>
2020-08-23 12:00:59 -0300

    Novo Metodo Lista() retorna array de string de Nomes

---
+++ Git/Models/Teste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Git.Models
+{
+    public class Teste
+    {
+        public string[] Lista()
+        {
+            List<string> listagem = new List<string>();
+            listagem.Add("João Carlos");
+            listagem.Add("Marta Olmedo");
+            listagem.Add("Lucas Olmedo");
+            string[] s = new string[3];
+            int i = 0;
+            listagem.ForEach(p => {
+                s[i] = p;
+                i++;
+            });
+            return s;
+        }
+    }
+}
